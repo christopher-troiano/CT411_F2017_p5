b37b27e3344800af2ed95bed2874b5173ac17d6d
christopher-troiano <31459582+christopher-troiano@users.noreply.github.com>
2017-09-05 19:25:31 -0400

    initial upload, homework

---
+++ Troiano_Week1_HW/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(1000, 1000);
+}
+
+function draw() {
+    background(45,247,136);
+    fill(0,0,0);
+    ellipse(499,499,718,718);
+   
+    fill(255,255,255);
+    ellipse(367,367,144,144);
+    
+    fill(255,255,255);
+    ellipse(633,367,144,144);
+    fill(0,0,0);
+    ellipse(367,367,88,88);
+    
+    fill(0,0,0);
+    ellipse(633,367,88,88);
+    
+    fill(255,157,250);
+    ellipse(499,627,240,240);
+    
+}
